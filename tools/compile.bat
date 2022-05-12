5566af18547f995211db7ecbd30295bd45612da5
m <mjkzyalt@gmail.com>
2022-05-11 19:46:23 -0500

    test branch merge 1 (#1)
    * fix withdrawl all internal
    
    * cleanup
    
    * test branch compiling
    
    * good one mikey
    
    * rename workflow
    
    * add notes [skip ci]
    
    * individual file support, int64_op, cleanup
    
    changes by https://github.com/fedddddd
    
    this requires latest version of t6-gsc-utils, which adds int64_op
    
    * Update README.md
    
    * Update README.md [skip ci]
    
    * gsc-tool compiling

--- tools/compile.bat
+++ tools/compile.bat
@@ -2,75 +2,27 @@
 setlocal enableDelayedExpansion
 setlocal enableExtensions
 
-:: Delete any gsc/zip/subfolders inside this folder just in case it was left over from a previous run.
+:: Clear out any gsc, zip, and tmp/ files from previous runs.
 del *.gsc /s /f /q > nul 2>&1
 del *.zip /s /f /q > nul 2>&1
-for /f "delims=" %%d in ('dir /s /b /ad ^| sort /r') do rd "%%d"  > nul 2>&1
+del /f /s /q tmp > nul
+rd /s /q tmp > nul
 
-:: Set SEARCHPATH.
+
+mkdir tmp
+:: Set variables.
 set "crt_dir=%~dp0"
 for %%I in ("%crt_dir%\..") do set "root=%%~fI"
-set "SEARCHPATH=%root%\src\gsc"
+set "SEARCHPATH=%root%\src"
+set FileName=%1
 
-IF /i "%1"=="onefile" goto onefile
-IF /i "%1"=="multifile" goto multifile
-echo Usage^: compile.bat onefile^|multifile filename.gsc^|filename.zip
-goto error
+:: Copy everything from ./src into %cd%
+robocopy %SEARCHPATH% %cd%\tmp\ /s /e > nul
 
-:onefile
-set FileName=%2
-for /D %%I in ("%SEARCHPATH%\*") do (
-    echo Error^: You cannot have folders in src^\gsc.
-    goto error
-)
-:: Generate raw GSC by combining all the .gsc files into one file.
-for /F %%x in ('dir /B/D %SEARCHPATH%') do (
-  type %SEARCHPATH%\%%x >> "uncompiled.gsc"
-)
-:: Compile the raw GSC into a single .gsc file.
-Compiler.exe uncompiled.gsc
-:: Delete the raw GSC + rename the compiled .gsc file.
-del /f uncompiled.gsc
-ren "uncompiled-compiled.gsc" "%FileName%"
-echo - Compiled GSC.
-EXIT /B 0
-
-:multifile
-set FileName=%2
-:: Generate raw GSC for root folder only.
-for /F %%x in ('dir /B/D/A:-D %SEARCHPATH%') do (
-  type %SEARCHPATH%\%%x >> %%x
-)
-:: Compile the raw GSC.
-for %%I in (%crt_dir%\*.gsc) DO (
-    Compiler.exe %%I
-    del /F %%I
-)
-start /wait PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {get-childitem *.gsc | foreach { rename-item $_ $_.Name.Replace(\"-compiled\", \"\") }}"
-
-:: Recur copy directories from scr\gsc to current dir.
-for /F %%x in ('dir /B/D/A:D %SEARCHPATH%') do (
-  mkdir %%x > nul 2>&1
-  xcopy /E /Y %SEARCHPATH%\%%x %%x > nul 2>&1
-)
-
-:: For any subfolders inside %crt_dir%, run compiler
-for /f "tokens=*" %%G in ('dir /b /s /a:d "%crt_dir%*"') do (
-    for %%I in (%%G\*.gsc) DO (
-    start /wait /D %%G %crt_dir%\Compiler.exe %%I
-  )
-  start /wait /D %%G PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {get-childitem *.gsc | foreach { Move-Item -force $_ $_.Name.Replace(\"-compiled\", \"\") }}"
-)
+gsc-tool.exe comp t6 %cd%\tmp\ > nul
 
 echo - Compiled GSC.
-start /wait 7za a %FileName% *.gsc -r
+
+:: ZIP all the gsc together
+start /wait 7za a %FileName% .\tmp\* > nul
 echo - ZIPd GSC.
-EXIT /B 0
-
-goto cleanexit
-
-:error
-EXIT /B 1
-
-:cleanexit
-EXIT /B 0
